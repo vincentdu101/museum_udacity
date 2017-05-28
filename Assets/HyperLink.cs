90be5af668ca2ed57198e8bca852919a0f1fa336
vincentdu101 <vincentdu101@gmail.com>
2017-05-28 00:41:15 -0700

    adding btn for initial click tests

---
+++ Assets/HyperLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperLink : MonoBehaviour {
+
+	public string link;
+
+	// Use this for initialization
+	void Start () {
+		Application.OpenURL ("www.google.com");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goToLink() {
+		Debug.Log ("test");
+		Application.OpenURL ("www.google.com");
+	}
+}
